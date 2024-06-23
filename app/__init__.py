a9fc1acebc0952565fb49b6ab8a5a7509f7c3a34
Muath Alsubhi <79430184+63G@users.noreply.github.com>
2024-06-23 06:22:31 +0300

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,7 @@
 from flask import Flask
-from dotenv import load_dotenv
-import os
-
-load_dotenv()  # Load environment variables from .env file
+from . import routes
 
 def create_app():
-    app = Flask(__name__, template_folder='../templates')  # Explicitly set the template folder
-    from . import routes
+    app = Flask(__name__)
     routes.init_app(app)
     return app
