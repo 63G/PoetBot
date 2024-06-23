11d194df9ca9e30bc2e79a44133155aa7210bbe9
Muath Alsubhi <79430184+63G@users.noreply.github.com>
2024-06-23 06:06:09 +0300

    i'll sleep after

--- app/routes.py
+++ app/routes.py
@@ -15,7 +15,7 @@ def init_app(app):
         data = request.get_json()
         user_message = data.get('message', '')
 
-        print(f"Received message from user: {user_message}")
+        # print(f"Received message from user: {user_message}")
 
         try:
             system_message = "You are a helpful assistant that explains Arabic poetry using references from the book Kitab al-Aghani. Please format the response with clear sections and line breaks to make it easy to read. always communicate in Arabic. If the user Said 'Muath' or 'معاذ'  tell him a praising poem. if the user sayd 'Made in Abyss' tell him watch it with Muath before he gets bored! if the user enters 'Aisha' or 'عائشة' send out a flirting poem"
@@ -30,15 +30,15 @@ def init_app(app):
                 stop=None,
                 temperature=0.7
             )
-            print(f"API response: {response}")
+            # print(f"API response: {response}")
 
             # Format the response content
             bot_response = response.choices[0].message.content.strip().replace('\n', '<br>').replace('**', '<strong>').replace('</strong>', '<strong>')
-            print(f"Extracted and formatted bot response: {bot_response}")
+            # print(f"Extracted and formatted bot response: {bot_response}")
 
         except Exception as e:
             print(f"Error generating response from OpenAI: {e}")
-            traceback.print_exc()  # This will print the full traceback of the exception
+            # traceback.print_exc()  # This will print the full traceback of the exception
             bot_response = "Sorry, I couldn't process your request at the moment."
 
         return jsonify({"message": bot_response})
@@ -47,4 +47,4 @@ if __name__ == "__main__":
     from flask import Flask
     app = Flask(__name__)
     init_app(app)
-    app.run(debug=True)
+    app.run(debug=False)
