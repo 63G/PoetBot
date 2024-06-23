964bd3699a2f8ffbe575f31fc672fb313fb738f1
Muath Alsubhi <79430184+63G@users.noreply.github.com>
2024-06-23 05:51:52 +0300

    heroku launch

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from app import create_app
-from dotenv import load_dotenv
-import os
+from flask import Flask
 
-# Load environment variables from .env file
-load_dotenv()
+app = Flask(__name__)
 
-app = create_app()
+@app.route('/')
+def hello():
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    flask_env = os.getenv('FLASK_ENV', 'production')  # Default to 'production' if not set
-    print("Current Working Directory:", os.getcwd())  # Print the current working directory
-    print("FLASK_ENV:", flask_env)  # Print the FLASK_ENV value
-    app.run(debug=flask_env != 'production')
+    app.run()
